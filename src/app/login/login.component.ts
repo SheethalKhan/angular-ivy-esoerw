946e442eca08585c3242e6234433d8435f5c3dd2
SheethalKhan <sheethalkhans06@gmail.com>
2022-01-23 17:42:19 +0530

    2 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,67 +12,40 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // this.arrayList=[
-    //   {
-    //     "product": "Caprese bag",
-    //     "description": "blue sling bag with a pink abstract design",
-    //     "price": 1000
-    //   },
-    //   {
-    //     "product": " Baggitbag",
-    //     "description": "blue sling bag with a pink abstract design",
-    //     "price": 1000
-    //   },
-    //   {
-    //     "product": "Lavie bag",
-    //     "description": "blue sling bag with a pink abstract design",
-    //     "price": 1000
-    //   },
-    //   {
-    //     "product": "Linos perroros bag",
-    //     "description": "blue sling bag with a pink abstract design",
-    //     "price": 1000
-    //   },
-    //   {
-    //     "product": " sheethal bag",
-    //     "description": "blue hjkjjkjk $%&**bbnmmhg sling bag with a pink abstract design",
-    //     "price": 18800
-    //   },
-    //   {
-    //     "product": "Rajan bag",
-    //     "description": "bhfjgkjgkjuyuttttttttttttttttttttttttt lue sling bag with a pink abstract design",
-    //     "price": 10
-    //   },
-    //   {
-    //     "product": "Ragavendran bag",
-    //     "description": "bluhjmjmjkjkje sling bag with a pink abstract design",
-    //     "price": 10850
-    //   },
-    //   {
-    //     "product": "adipoli bag",
-    //     "description": "blue slkjkj fgfdhgfh yjy 566451651 hing bag with a pink abstract design",
-    //     "price": 789
-    //   },
-    //   {
-    //     "product": "sundari bag",
-    //     "description": "bluehghg gdhgfh fhfhj sling bag with a pink abstract design",
-    //     "price": 5500
-    //   },
-    //   {
-    //     "product": "manavalan bag",
-    //     "description": " bag with a pink abstract design",
-    //     "price": 10800
-    //   },
-    //   {
-    //     "product": "poli bag",
-    //     "description": "blue sling pink abstract design",
-    //     "price": 1400
-    //   },
-    //   {
-    //     "product": "doluma aaluma bag",
-    //     "description": "blue sling bag with a  design",
-    //     "price": 184
-    //   }
-    // ]
+    this.arrayList = [
+      {
+        name: 'Nishant',
+        age: 25
+      },
+      {
+        name: 'Shailesh',
+        age: 45
+      },
+      {
+        name: 'Abhishek',
+        age: 36
+      },
+      {
+        name: 'Akshay',
+        age: 65
+      },
+      {
+        name: 'Ashish',
+        age: 12
+      },
+      {
+        name: 'Uday',
+        age: 31
+      },
+      {
+        name: 'Mayank',
+        age: 45
+      },
+      {
+        name: 'Raju',
+        age: 74
+      },
+    ]
+  
   }
 }
